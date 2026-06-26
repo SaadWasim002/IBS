b6a5461f8f9398c5e1bc612ee201403c0833d6ce
SaadWasim02 <saadwasim444@gmail.com>
2026-06-27 02:27:59 +0530

    file initialization

---
+++ src/test/java/com/upi/IBS/IbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upi.IBS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
